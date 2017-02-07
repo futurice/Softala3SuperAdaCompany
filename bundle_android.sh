c2dff28a4c8059e2f105544c771f9350640c92ba
Rasmus Eskola <fruitiex@gmail.com>
2017-02-07 20:11:59 +0200

    Rename app output .apk

--- bundle_android.sh
+++ bundle_android.sh
@@ -2,7 +2,7 @@ cd "$(dirname $0)"
 set -e
 
 echo 'Cleaning previous build...'
-rm superada.apk > /dev/null 2>&1 || true
+rm superada-companyapp.apk > /dev/null 2>&1 || true
 
 (cd android &&
     ./gradlew clean &&
@@ -11,8 +11,8 @@ rm superada.apk > /dev/null 2>&1 || true
     ./gradlew assembleRelease
 )
 
-(cp android/app/build/outputs/apk/app-release.apk superada.apk &&
-    echo -e '\nSuccess! You can find the built APK in "superada.apk"\n'
+(cp android/app/build/outputs/apk/app-release.apk superada-companyapp.apk &&
+    echo -e '\nSuccess! You can find the built APK in "superada-companyapp.apk"\n'
 ) ||
 (echo -e '\nERROR!\nUnable to find generated app-release.apk! Most likely app signing failed, check keystore setup in README.md\n' &&
     exit 1
